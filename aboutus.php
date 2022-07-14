7afbb7a0b0011d17c83916f5f886edda4689af8e
aishwaryapatilp <aishwaryapatilp311@gmail.com>
2022-07-14 16:58:58 +0530

    student

---
+++ aboutus.php
@@ -0,0 +1,19 @@
+<?php include('topbar.php');?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/aboutus.css">
+</head>
+<body>
+
+<div class="about-section">
+  <h1>Student Result Management System</h1>
+  <p>We are a team of 3 members colaborating in a project and made all possible efforts  to make it hope you like it.</p>
+  <p> hope this may give us a good marks.</p>
+</div>
+
+
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,22 @@
+<?php include('topbar.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title></title>
+</head>
+<body style="background-color: skyblue;">
+    <br><br><br>
+    <div class="box" >
+    	<center><u><h1>Contact Us</h1></u></center>
+         <ul style="font-size: 30px;">
+         	<li>location - The Oxford College Of Engineering</li>
+         	<li>At - Banglore</li>
+            <li>Pin - 560 068</li>
+         	<li>contact no - 7899145545,9901196419
+         	</li>
+         </ul>
+         <center><h2>Thank you</h2></center>
+    </div>
+</body>
+</html>
